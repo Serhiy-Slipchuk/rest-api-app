c94d0656ec705bdbacb4500e5bfda24da5e2e3eb
Serhiy Slipchuk <slipchuks@ukr.net>
2023-07-30 02:05:02 +0300

    Step 03

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const usersRouter = require("./routes/api/users")
 
 require("dotenv").config();
 
-const { middlewarePostPut, middlewarePatch, middlewareUsers } = require("./middlewares");
+const { middlewarePostPut, middlewarePatch, middlewareUsers, middlewareAuth } = require("./middlewares");
 
 const app = express();
 
@@ -19,7 +19,7 @@ app.use(logger(formatsLogger));
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/contacts", middlewarePostPut, middlewarePatch, contactsRouter);
+app.use("/api/contacts", middlewarePostPut, middlewarePatch, middlewareAuth, contactsRouter);
 app.use("/api/users", middlewareUsers, usersRouter);
 
 app.use((req, res) => {
--- middlewares/index.js
+++ middlewares/index.js
@@ -1,9 +1,11 @@
 const middlewarePostPut = require("./middlewarePostPut");
 const middlewarePatch = require("./middlewarePatch");
 const middlewareUsers = require("./middlewareUsers");
+const middlewareAuth = require("./middlewareAuth");
 
 module.exports = {
     middlewarePostPut,
     middlewarePatch,
-    middlewareUsers
+    middlewareUsers,
+    middlewareAuth
 }
