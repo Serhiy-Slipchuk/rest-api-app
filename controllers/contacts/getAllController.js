21c25c182f18539bde5a2d3d23a6f284d04c2c66
Serhiy Slipchuk <slipchuks@ukr.net>
2023-07-31 07:06:53 +0300

    update owners

--- controllers/contacts/getAllController.js
+++ controllers/contacts/getAllController.js
@@ -2,26 +2,27 @@ const { Contact } = require("../../models/contact");
 const { createGetAllResponse } = require("../../services")
 
 const getAllController = async (req, res, next) => {
-  const {page, limit, favorite} = req.query;
+  const { _id: owner } = req.user;
+  const { page, limit, favorite } = req.query;
   const pageNumber = Number(page);
   const pageLimit = Number(limit);
   const skip = (pageNumber - 1) * pageLimit;
 
   try {
     if (page && limit && favorite) {
-      const contacts = await Contact.find({favorite: !!favorite}).skip(skip).limit(pageLimit);
+      const contacts = await Contact.find({owner, favorite: !!favorite}).skip(skip).limit(pageLimit);
       createGetAllResponse(res, contacts);
 
     } else if (page && limit) {
-      const contacts = await Contact.find().skip(skip).limit(pageLimit).exec();
+      const contacts = await Contact.find({owner}).skip(skip).limit(pageLimit).exec();
       createGetAllResponse(res, contacts);
 
     } else if (favorite) {
-      const contacts = await Contact.find({favorite: !!favorite})
+      const contacts = await Contact.find({owner, favorite: !!favorite})
       createGetAllResponse(res, contacts);
 
     } else {
-      const contacts = await Contact.find();
+      const contacts = await Contact.find({owner});
       createGetAllResponse(res, contacts);
     } 
   } catch (error) {
